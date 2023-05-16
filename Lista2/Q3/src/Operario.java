bd4a3477a8586d30733821dece4c3a317edb1881
Ianco Ferraz <111300798+iancoF17@users.noreply.github.com>
2023-05-15 22:21:12 -0300

    Create Operario.java

---
+++ Lista2/Q3/src/Operario.java
@@ -0,0 +1,26 @@
+public class Operario extends Empregado {
+    private double valorProducao;
+    private double comissao;
+
+    public double getValorProducao() {
+        return valorProducao;
+    }
+
+    public void setValorProducao(double valorProducao) {
+        this.valorProducao = valorProducao;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        double salarioLiquido = super.calcularSalario() + (valorProducao * (comissao / 100));
+        return salarioLiquido;
+    }
+}
